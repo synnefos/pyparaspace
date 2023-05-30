98d495f8a569fe07588da70364e1f1a1ba694b48
Bjørnar Luteberget <bjornar.luteberget@sintef.no>
2023-05-30 11:25:47 +0200

    paraspace: correctly infer timeline transition when timeline self-reference is given as singleton set

--- testPyParaspace.py
+++ testPyParaspace.py
@@ -2,24 +2,59 @@ import math
 import unittest
 import pyparaspace
 
+
 def test_pyparaspace():
     problem = pyparaspace.Problem(
         groups=[],
-        timelines=[pyparaspace.Timeline(name="obj", values=[pyparaspace.Value(name="s1", conditions=[], duration=(5,6), capacity=0), pyparaspace.Value(name="s2", conditions=[pyparaspace.Condition(temporal_relationship=pyparaspace.TemporalRelationship.MetBy, amount=0, objects=["obj"], value="s1")], duration=(1,None), capacity=0)])],
-        tokens=[pyparaspace.Token(timeline_name="obj", value="s2", const_time=pyparaspace.goal(), capacity=0, conditions=[])])
+        timelines=[
+            pyparaspace.Timeline(
+                name="obj",
+                values=[
+                    pyparaspace.Value(
+                        name="s1", conditions=[], duration=(5, 6), capacity=0
+                    ),
+                    pyparaspace.Value(
+                        name="s2",
+                        conditions=[
+                            pyparaspace.Condition(
+                                temporal_relationship=pyparaspace.TemporalRelationship.MetBy,
+                                amount=0,
+                                objects=["obj"],
+                                value="s1",
+                            )
+                        ],
+                        duration=(1, None),
+                        capacity=0,
+                    ),
+                ],
+            )
+        ],
+        tokens=[
+            pyparaspace.Token(
+                timeline_name="obj",
+                value="s2",
+                const_time=pyparaspace.goal(),
+                capacity=0,
+                conditions=[],
+            )
+        ],
+    )
 
     solution = pyparaspace.solve(problem)
     print(f"Solution: {solution}")
     assert len(solution.tokens) == 2, "Number of tokens should be 2"
 
-    token1 = solution.tokens[1]
-    token2 = solution.tokens[0]
+    token1 = solution.tokens[0]
+    token2 = solution.tokens[1]
     assert token1.value == "s1", "Token value 1 should be s1"
     assert token2.value == "s2", "Token value 2 should be s2"
 
-    assert token1.end_time - token1.start_time >= 5. and token1.end_time - token1.start_time <= 6.
+    assert (
+        token1.end_time - token1.start_time >= 5.0
+        and token1.end_time - token1.start_time <= 6.0
+    )
     assert abs(token1.end_time - token2.start_time) < 1e-5
-    assert math.isinf(token2.end_time)
+
 
 if __name__ == "__main__":
     test_pyparaspace()
