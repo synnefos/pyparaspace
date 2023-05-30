0ddfc8795578c0e703c87a48455dbdd756b6bf08
Eirik Kjeken <Eirik.Kjeken@sintef.no>
2023-05-30 10:45:35 +0200

    Add test for pyparaspace

--- testPyParaspace.py
+++ testPyParaspace.py
@@ -1,19 +1,25 @@
+import math
+import unittest
 import pyparaspace
 
 def test_pyparaspace():
     problem = pyparaspace.Problem(
         groups=[],
-        timelines=[pyparaspace.Timeline(name="obj", values=[pyparaspace.Value(name="s1", conditions=[], duration=(5,6), capacity=0), pyparaspace.Value(name="s2", conditions=[pyparaspace.Condition(temporal_relationship=pyparaspace.TemporalRelationship.MetBy, amount=0, object=["obj"], value="s1")], duration=(1,None), capacity=0)])],
+        timelines=[pyparaspace.Timeline(name="obj", values=[pyparaspace.Value(name="s1", conditions=[], duration=(5,6), capacity=0), pyparaspace.Value(name="s2", conditions=[pyparaspace.Condition(temporal_relationship=pyparaspace.TemporalRelationship.MetBy, amount=0, objects=["obj"], value="s1")], duration=(1,None), capacity=0)])],
         tokens=[pyparaspace.Token(timeline_name="obj", value="s2", const_time=pyparaspace.goal(), capacity=0, conditions=[])])
 
     solution = pyparaspace.solve(problem)
     print(f"Solution: {solution}")
-    print(f"Number of tokens should be 2. Actual: {len(solution.tokens)}")
+    assert len(solution.tokens) == 2, "Number of tokens should be 2"
+
     token1 = solution.tokens[1]
     token2 = solution.tokens[0]
-    print(f"Token value 1 should be s1. Actual value: {token1.value}")
-    print(f"Token value 2 should be s2. Actual value: {token2.value}")
-    print(f"Should be true. Actual value: {token1.end_time - token1.start_time >= 5. and token1.end_time - token1.start_time <= 6.}")
-    print(f"Should be true. Actual value: {abs(token1.end_time - token2.start_time) < 1e-5}")
-    #assert!(token2.end_time.is_infinite());
+    assert token1.value == "s1", "Token value 1 should be s1"
+    assert token2.value == "s2", "Token value 2 should be s2"
 
+    assert token1.end_time - token1.start_time >= 5. and token1.end_time - token1.start_time <= 6.
+    assert abs(token1.end_time - token2.start_time) < 1e-5
+    assert math.isinf(token2.end_time)
+
+if __name__ == "__main__":
+    test_pyparaspace()
